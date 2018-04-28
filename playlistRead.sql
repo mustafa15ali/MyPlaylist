c5bc6092ff9191b9b1e6daf322a861a0d8d36a4f
Mustafa Farah <mustafa.farah@outlook.com>
2018-04-28 12:37:31 -0500

    adding code fix

--- playlistRead.sql
+++ playlistRead.sql
@@ -3,13 +3,13 @@ CREATE DATABASE playlistDB;
 
 USE playlistDB;
 
-CREATE TABLE songs(
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(45) NULL,
-  artist VARCHAR(45) NULL,
-  genre VARCHAR(45) NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE songs(
+--   id INT NOT NULL AUTO_INCREMENT,
+--   title VARCHAR(45) NULL,
+--   artist VARCHAR(45) NULL,
+--   genre VARCHAR(45) NULL,
+--   PRIMARY KEY (id)
+-- );
 
 INSERT INTO songs (title, artist, genre)
 VALUES ("My Way", "Ella Mai", "R&B/Soul");
